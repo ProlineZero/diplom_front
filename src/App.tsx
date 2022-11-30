5ff85ace43b09473283da6fe9782b3215de2abc3
sxrgey <serezha_vin_19@mail.ru>
2022-11-30 16:27:18 +0300

    nav

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {Route, Routes} from 'react-router-dom'
 import { useEffect } from 'react'
 import { RegistrationPage } from './pages/RegistrationPage'
 import { MainPage } from './pages/MainPage'
+import { AuthorizationPage } from './pages/AuthorizationPage'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       
       <Routes>
         <Route path="/" element={ <MainPage/> } />
+        <Route path="/authorization" element={ <AuthorizationPage/> } />
         <Route path="/registration" element={ <RegistrationPage/> } />
       </Routes> 
 
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,30 +1,12 @@
 import React from 'react'
 import { InputString } from '../components/InputString'
+import { Navigation } from '../components/Navigation'
 
 export function MainPage() {
   return (
 
     <>
-      <div
-        className = "fixed bg-gray-100 w-5/12 h-1/2 left-1/2 -translate-x-1/2 top-1/3 -translate-y-1/2 rounded-3xl">
-      
-      
-
-      <div className="container w-1/2 space-y-3 fixed inline-block text-left left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
-        <h2 className="text-3xl text-center text-purple-500">Online Books</h2>
-          <h1 className='text-2xl text-center text-gray-500'>Вход</h1>
-          <InputString title='E-mail' placeholder = 'Введите E-mail' name = 'email' />
-          <InputString title='Пароль' placeholder = 'Введите пароль' name = 'password' />
-          <button className=" w-full px-2 py-1 transition ease-in duration-200 uppercase rounded-full text-purple-700 hover:bg-purple-600 hover:text-white border-2 border-purple-700 focus:outline-none">
-            Войти
-          </button>
-        <h1 className='text-gray-500 text-sm text-center overline'>Нет аккаунта в Online Books?</h1>
-        <button type="button" className="w-full px-2 py-1 transition ease-in duration-200 uppercase rounded-full bg-purple-100 text-purple-500 border-purple-100 hover:bg-purple-200 hover:border-purple-200 border-2 focus:outline-none "
-        onClick= {() => window.location.assign('http://localhost:3000/registration')}>
-          Зарегистрироваться
-        </button>
-      </div>
-    </div>
+      <Navigation/>
     </>
     
   )
