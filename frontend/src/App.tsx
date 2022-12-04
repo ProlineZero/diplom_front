6f421de4e9ab7897e29eb28635998f2478c3b8b3
sxrgey <serezha_vin_19@mail.ru>
2022-12-04 19:09:01 +0300

    need to finish scrolling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import {Route, Routes} from 'react-router-dom'
 import { useEffect } from 'react'
 import { RegistrationPage } from './pages/RegistrationPage'
-import { MainPage } from './pages/MainPage'
 import { AuthorizationPage } from './pages/AuthorizationPage'
 import { FavoritesPage } from './pages/FavoritesPage'
 import { CarsPage } from './pages/CarsPage'
--- frontend/src/pages/CarsPage.tsx
+++ frontend/src/pages/CarsPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Card } from '../components/Card'
 import { Navigation } from '../components/Navigation'
 
 export function CarsPage() {
@@ -6,9 +7,26 @@ export function CarsPage() {
 
     <>
       <Navigation numPressedBtn = {0}/>
-      <div className= 'left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 fixed bg-gray-200 h-[200px] w-[200px]'>
-        <h1 className='text-center'>Машины</h1>
-      </div>
+      <section className="">
+      {/* <div className="bg-gray-300 mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8"> */}
+        <div className="bg-gray-300 static w-5/6 h-[90%] left-[17%] top-[10%]">
+          <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+          </div>
+        </div>
+        </section>
 
     </>
     
